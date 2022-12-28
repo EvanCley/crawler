cfcb86ead6cb98c9ab66644df27ee4430ca0f00c
EvanCley <caoyifan1a2b@163.com>
2022-12-28 16:53:56 +0800

    add Fetch function

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module crawler
 
 go 1.17
+
+require (
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -30,4 +36,42 @@ func main() {
 	}
 
 	fmt.Println("body:", string(body))
+
+	numLinks := strings.Count(string(body), "<a")
+	fmt.Printf("homepage has %d links!\n", numLinks)
+
+	exist := strings.Contains(string(body), "疫情")
+	fmt.Printf("当前首页是否存在疫情相关的新闻：%v\n", exist)
+
+}
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code: %d", resp.StatusCode)
+	}
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DetermineEncoding(bodyReader)                            // 单独封装了 DeterminEncoding 函数来检测并返回当前 HTML 文本的编码格式
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder()) // 最后，transform.NewReader 用于将 HTML 文本从特定编码转换为 UTF-8 编码，从而方便后续的处理。
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+
+	// 如果返回的 HTML 文本小于 1024 字节，我们认为当前 HTML 文本有问题，直接返回默认的 UTF-8 编码就好了。
+	if err != nil {
+		fmt.Printf("fetch error: %v\n", err)
+		return unicode.UTF8
+	}
+	// charset.DetermineEncoding 函数用于检测并返回对应 HTML 文本的编码。
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
